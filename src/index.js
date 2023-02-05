651227fefd6c17a2287dd3d55c89d2b7ee58dc27
Omer Dotchamou <ddaniomer@gmail.com>
2023-02-05 01:15:14 +0100

    Layout files and route created

--- src/index.js
+++ src/index.js
@@ -5,24 +5,26 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Logement from './pages/Error';
 import Error from './pages/Error';
+
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/logement" element={<Logement />} />
         <Route path="/*" element={<Error />} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+

b8c04248bca0a87d65849b87383e6b6d4c894392
Omer Dotchamou <ddaniomer@gmail.com>
2023-02-05 03:21:26 +0100

    Header layout set up

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
 import './header.scss'
+
+import LOGO from '../../assets/LOGO.svg'
 
 const Header = () => {
     return (
-        <header>
-            
+        <header className="header">
+            <img src={LOGO} alt="kasa logo" className="header__logo"/>
+            <nav className="header__nav">
+                <Link to="/" className="nav__link nav__link--active">Accueil</Link>
+                <Link to="/about" className="nav__link">A Propos</Link>
+            </nav>
         </header>
     )
 }

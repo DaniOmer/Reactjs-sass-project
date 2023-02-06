e29719306d36d7ce4de72760da9a92ec75506635
Omer Dotchamou <ddaniomer@gmail.com>
2023-02-06 12:10:40 +0100

    Error page set up

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,11 +1,14 @@
+
 import './error.scss'
 
-const Error = () => {
+function Error() {
     return (
-        <div>
-            
-        </div>
+      <main className="error">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'hexiste pas.</p>
+        <a href="/">Retournez sur la page d'accueil</a>
+      </main>
     )
-}
-
-export default Error
+  }
+  
+  export default Error

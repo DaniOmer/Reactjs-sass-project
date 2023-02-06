bf6f4e23381b6a7747f68acd2db506ae95d57a62
Omer Dotchamou <ddaniomer@gmail.com>
2023-02-06 11:57:59 +0100

    Logement Card component set up

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,13 +3,14 @@ import './card.scss'
 import { Link } from 'react-router-dom'
 import { logements } from '../../data/logements'
 
-const Card = () => {
+/* Permet de générer les cartes des logements */
+function Card () {
     return (
         <section className='logements'>
 			<div className='logements__list'>
 				{logements.map(({ id, cover, title }) =>
 					<article key={id} className='logements__list__content'>
-						<Link to={`/location?id=${id}`}>
+						<Link to={`/logement?id=${id}`}>
 							<div className="logements__list__content__bg"></div>
 							<img className='logements__list__content__cover' src={cover} alt={`Logement ${title}`} />
 							<p className="logements__list__content__name">{title}</p>
